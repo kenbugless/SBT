88de6afd8f1d56161db7929dcf3429c9f899d98c
Hyunil Lee <kenbugless@gmail.com>
2023-05-23 02:00:13 +0900

    second

--- soulbound.sol
+++ soulbound.sol
@@ -1,46 +1,77 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: GPL-3.0
 
-import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
+pragma solidity ^0.8.0;
 
-contract SoulBoundTest is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract Erc2005 {
+    string private _nickName;
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => string) private _tokenURIs;
+    uint256 private _tokenIdCounter;
+    uint256 private _age;
+    uint256 private _storeId;
 
-    Counters.Counter private _tokenIdCounter;
-
-    constructor() ERC721("SoulBoundTest", "SBT") {}
-
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    constructor(string memory nickName_, uint256 age_, uint256 storeId_) {
+        _nickName = nickName_;
+        _age = age_;
+        _storeId = storeId_;
     }
 
-    function _beforeTokenTransfer(
-    address from, 
-    address to, 
-    uint256 tokenId
-    ) internal override virtual {
-    require(from == address(0), "Err: token transfer is BLOCKED"); 
-    super._beforeTokenTransfer(from, to, tokenId);  
+    function nickName() public view returns (string memory) {
+        return _nickName;
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_owners[tokenId] != address(0), "non-exist for tokenURI on token");
+        return _tokenURIs[tokenId];
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0), "non-exist for balance on address");
+        return _balances[owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "non-exist for the query on token");
+        return owner;
+    }
+
+    function mint(address to, string memory tokenURI) public returns (uint256) {
+        require(to != address(0), "non-exist address to mint");
+
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter += 1;
+
+        _owners[tokenId] = to;
+        _balances[to] += 1;
+        _tokenURIs[tokenId] = tokenURI;
+
+        return tokenId;
+    }
+
+    function burn(uint256 tokenId) public {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "non-exist token to burn");
+
+        delete _owners[tokenId];
+        _balances[owner] -= 1;
+        delete _tokenURIs[tokenId];
+    }
+
+    function isStore() public view returns (bool) {
+        return _storeId != 0;
+    }
+
+    function storeId() public view returns (uint256) {
+     if (_storeId != 0) {
+        return _storeId;
+    }
+    return 0;
+}
+
+    function setStoreId(uint256 storeId_) public {
+        require(!isStore(), "Can only set  ID once");
+        _storeId = storeId_;
     }
 }
